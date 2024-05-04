4a6af6e9b0e53a95e7ad0d07cc2f1a9bc82c0ac2
John Schember <john@nachtimwald.com>
2024-05-03 20:21:12 -0400

    Change default config for cache to no cache because the memory cache will only really work in test.

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 REQUESTS_PROXIED = False
 IPGEO_API_KEY = None
 CACHE_CONFIG = {
-    'CACHE_TYPE': 'SimpleCache',
-    'CACHE_DEFAULT_TIMEOUT': 60 * 60 * 24 * 7 * 2 # 2 weeks
+    'CACHE_TYPE': 'NullCache'
 }
